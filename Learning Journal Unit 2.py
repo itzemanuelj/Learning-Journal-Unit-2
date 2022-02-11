1b0b88b438f21c8fd0b253d952567f78dd220515
Emanuel Joness <itzemanuelj@gmail.com>
2022-02-10 21:29:49 -0500

    Create Learning Journal Unit 2.py

---
+++ Learning Journal Unit 2.py
@@ -0,0 +1,46 @@
+#Learning Journal Unit 2
+
+#Part 1
+
+#The volume of a sphere is 4/3πr3, where π has the value of "pi" given in Section 2.1 of your textbook. Write a function called print_volume (r) that takes an argument for the radius of the sphere, and prints the volume of the sphere.
+
+#Call your print_volume function three times with different values for radius.
+
+#Include all of the following in your Learning Journal:
+
+#The code for your print_volume function.
+#The inputs and outputs to three calls of your print_volume.
+
+
+def print_volume(r):
+    
+    return print((4 / 3) * 3.14 * (r ** 3))
+
+print_volume(1)
+print_volume(2)
+print_volume(3)
+
+#>>> print_volume(1)
+#4.1866666666666665
+#>>> print_volume(2)
+#33.49333333333333
+#>>> print_volume(3)
+#113.03999999999999
+#>>>
+
+
+
+#Part 2
+#Write your own function that illustrates a feature that you learned in this unit. 
+#The function must take at least one argument. The function should be your own creation, not copied from any other source. 
+#Do not copy a function from your textbook or the Internet.
+
+a = "CS1101" 
+b = " Programming Fundamentals"
+c = " AY2022-T3 "
+z = a + b + c
+print (z)
+
+#CS1101 Programming Fundamentals AY2022-T3 
+
+#yon can assign values to a funtion and concatenate them together to make a new function
